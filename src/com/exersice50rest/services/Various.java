f5e011ff8e28552cf42c622dd755fd8a4fc3a884
Prog998 <j.j.virus@hotmail.com>
2020-02-11 10:34:45 -0600

    Rest service with a message was created.

---
+++ src/com/exersice50rest/services/Various.java
@@ -0,0 +1,14 @@
+package com.exersice50rest.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/services")
+public class Various {
+	
+	@GET
+	@Path("/obligame")
+	public String mensajitu() {
+		return "Obligame Prro!!!";
+	} 
+}
